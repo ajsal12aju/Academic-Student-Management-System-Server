8939432fad040ea880c55679084faf1932eb8cdc
Ajsal <ajsal@frugalscientific.com>
2025-02-14 00:04:14 +0530

    update

--- index.js
+++ index.js
@@ -27,7 +27,6 @@ const limiter = rateLimit({
 }); 
 app.use(limiter);
 
-// Database Connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB"))
   .catch(err => console.error("MongoDB Connection Error:", err));
